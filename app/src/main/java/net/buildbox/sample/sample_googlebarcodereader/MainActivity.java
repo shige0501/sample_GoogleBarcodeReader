1c52bb51003afbeac748e7d7de294b12b2cf0b4b
Koji Shigemura <shige0501@gmail.com>
2016-04-27 22:46:47 +0900

    Googleのバーコード読みこみAPIの実装追加

--- app/src/main/java/net/buildbox/sample/sample_googlebarcodereader/MainActivity.java
+++ app/src/main/java/net/buildbox/sample/sample_googlebarcodereader/MainActivity.java
@@ -1,13 +1,55 @@
 package net.buildbox.sample.sample_googlebarcodereader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import net.buildbox.sample.sample_googlebarcodereader.google.BarcodeCaptureActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int BARCODE_CAPTURE_REQUEST = 0x0001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.barcodeScanButton)
+    public void onBarcodeScanClick() {
+        Intent intent = new Intent(this, BarcodeCaptureActivity.class);
+        intent.putExtra(BarcodeCaptureActivity.AutoFocus, true);
+        intent.putExtra(BarcodeCaptureActivity.UseFlash, true);
+
+        startActivityForResult(intent, BARCODE_CAPTURE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BARCODE_CAPTURE_REQUEST) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    Toast.makeText(this, barcode.displayValue, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "バーコードがキャプチャ出来ませんでした", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "バーコードの読み込みに失敗しました", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
